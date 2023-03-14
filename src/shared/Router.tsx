c9918a53c0e8e9577ab8857aa7499b03a8c2ff00
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-15 04:35:03 +0900

    feat: contentWrapper Router 적용

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,21 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Home } from 'pages';
 import { ContentWrapper } from 'shared';
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ContentWrapper>
-              <Home />
-            </ContentWrapper>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <ContentWrapper>
+            <Home />
+          </ContentWrapper>
+        }
+      />
+    </Routes>
   );
 }
 
