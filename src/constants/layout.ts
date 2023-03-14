997bb9a0364f2d954bae3c6d31fe864edc799809
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-15 06:06:37 +0900

    refactor: navLink 값 소프트코드화

--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -1,5 +1,11 @@
 export const NAV = Object.freeze({
-  WIDTH: 108,
-  BORDER_WIDTH: 1,
-  BACKGROUND: '#D9D9D9',
+  WIDTH: 88 as const,
+  BORDER_WIDTH: 1 as const,
+  GAP: 24,
+});
+
+export const NAVLINK = Object.freeze({
+  WIDTH: 60 as const,
+  HEIGHT: 60 as const,
+  BORDER_RADIUS: 10 as const,
 });
