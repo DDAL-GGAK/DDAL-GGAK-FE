47513252b458f25315b1ba9d0d59d2be7c1794fb
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-15 01:17:42 +0900

    feat: home 컴포넌트 추가 및 index 설정

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+function Home() {
+  return <Wrapper>Home</Wrapper>;
+}
+
+export default Home;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
