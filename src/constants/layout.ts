2b2777d4456c3b089927f479d06697ae5c325f80
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-16 20:49:06 +0900

    refactor: constant index파일 추가

--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -1,7 +1,12 @@
-export const NAV = Object.freeze({
-  WIDTH: 88 as const,
+export const SIDE_NAV = Object.freeze({
+  WIDTH: 108 as const,
   BORDER_WIDTH: 1 as const,
   GAP: 24,
+});
+
+export const TOP_NAV = Object.freeze({
+  HEIGHT: 100 as const,
+  BORDER_WIDTH: 1 as const,
 });
 
 export const NAVLINK = Object.freeze({
