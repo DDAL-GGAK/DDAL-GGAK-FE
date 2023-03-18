29b4733068114a0cf02efd2a45f5711542a3dbba
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-18 20:51:54 +0900

    feat: menu icon 추가

---
+++ src/assets/icons/Menu.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export default function Menu() {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </Svg>
+  );
+}
+
+const Svg = styled.svg`
+  width: 30px;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
