c7ccad5bcaf969b3c2e825e48e8a7822a4c9a3a8
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-18 18:33:39 +0900

    chore: globalTheme 수정

--- src/styles/style.d.ts
+++ src/styles/style.d.ts
@@ -9,19 +9,6 @@ declare module 'styled-components' {
     pointColor: string;
     transitionOption: string;
 
-    /* ColorPalette */
-    PALETTE_GRAY: string;
-    PALETTE_50: string;
-    PALETTE_100: string;
-    PALETTE_200: string;
-    PALETTE_300: string;
-    PALETTE_400: string;
-    PALETTE_500: string;
-    PALETTE_600: string;
-    PALETTE_700: string;
-    PALETTE_800: string;
-    PALETTE_900: string;
-
     /* nav */
     navBackground: string;
     navLinkBackground: string;
