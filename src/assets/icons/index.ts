9f7e3225fa790f1895a852d9a4e8c19c08fdbcb0
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-19 01:40:38 +0900

    feat: config 컴포넌트 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,4 @@ export { default as Search } from './Search';
 export { default as Logo } from './Logo';
 export { default as Menu } from './Menu';
 export { default as Add } from './Add';
+export { default as ConfigSvg } from './ConfigSvg';
