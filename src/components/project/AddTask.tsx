99ad21162b0dbcb6f2dc86fc0bc1420fdf8fb50f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-23 21:55:51 +0900

    feat: addTask 컴포넌트 추가

--- src/components/project/AddTask.tsx
+++ src/components/project/AddTask.tsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
+import { Add } from 'assets/icons';
 
 export default function AddTask() {
-  return <Wrapper>1</Wrapper>;
+  return (
+    <Wrapper>
+      <Add />
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
   background: ${({ theme }) => theme.navBackground};
+  :hover {
+    cursor: pointer;
+  }
 `;
