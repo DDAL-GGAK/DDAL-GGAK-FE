dbe53a91e12993806fcb183b29e340911a5cccb0
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-24 15:56:33 +0900

    feat: user 컴포넌트 추가

---
+++ src/pages/User.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default function User() {
+  return <Wrapper>1</Wrapper>;
+}
+
+const Wrapper = styled.div``;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as Project } from './Project';
 export { default as Login } from './Login';
 export { default as Signup } from './Signup';
 export { default as Task } from './Task';
+export { default as User } from './User';
