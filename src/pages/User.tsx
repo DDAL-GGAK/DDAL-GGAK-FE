e4990e45b2c6a924c326119b703e425adf48ef73
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-24 18:47:39 +0900

    feat: userDetail API 연결

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,7 +1,21 @@
 import styled from 'styled-components';
 import { CONTENT } from 'constants/';
+import { getUserData } from 'api';
+import { useEffect, useState } from 'react';
 
 export default function User() {
+  const [userData, setUserData] = useState();
+
+  console.log(userData);
+
+  const onMountHandler = async () => {
+    const { data } = await getUserData();
+    setUserData(data);
+  };
+
+  useEffect(() => {
+    onMountHandler();
+  }, []);
   return <Wrapper>1</Wrapper>;
 }
 
