61ce3eb8dc571ef24066a5f8a98e4e650eefde1c
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-24 23:29:20 +0900

    refactor: upload 컴포넌트 분리

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -2,12 +2,11 @@ import styled from 'styled-components';
 import { CONTENT } from 'constants/';
 import { getUserData } from 'api';
 import { useEffect, useState } from 'react';
+import { UserDataForm } from 'types';
+import { UploadProfile } from 'components/user';
 
 export default function User() {
-  const [userData, setUserData] = useState();
-
-  console.log(userData);
-
+  const [userData, setUserData] = useState<UserDataForm>();
   const onMountHandler = async () => {
     const { data } = await getUserData();
     setUserData(data);
@@ -16,15 +15,68 @@ export default function User() {
   useEffect(() => {
     onMountHandler();
   }, []);
-  return <Wrapper>1</Wrapper>;
+
+  return (
+    <Wrapper>
+      <LeftWrapper>
+        <div>{userData?.email}</div>
+        <div>myAccount</div>
+        <div>nav1</div>
+        <div>nav2</div>
+        <div>nav3</div>
+      </LeftWrapper>
+      <RightWrapper>
+        <AccountWrapper>
+          <TextL>Account</TextL>
+          <div>{userData?.email}</div>
+        </AccountWrapper>
+        <ProfileWrapper>
+          <TextM>Profile</TextM>
+          <UploadProfile imageSrc={userData?.profile} />
+        </ProfileWrapper>
+      </RightWrapper>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
   height: ${CONTENT.HEIGHT};
   display: flex;
-  align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   transition: ${({ theme }) => theme.transitionOption};
-  background: ${({ theme }) => theme.transparentColor};
   border-radius: 10px;
+`;
+
+const LeftWrapper = styled.div`
+  width: 200px;
+  border-right: solid 1px ${({ theme }) => theme.borderColor};
+  box-sizing: border-box;
+  transition: ${({ theme }) => theme.transitionOption};
+`;
+
+const RightWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 35px;
+  width: calc(100% - 200px);
+  background: rgba(0, 0, 0, 0.1);
+`;
+
+const AccountWrapper = styled.div``;
+
+const TextL = styled.div`
+  font-size: 25px;
+  font-weight: 600;
+`;
+
+const TextM = styled.div`
+  font-size: 17.5px;
+  font-weight: 600;
+`;
+
+const ProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
 `;
