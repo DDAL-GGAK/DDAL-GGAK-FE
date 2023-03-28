bb9b708bba00bf7e9766eabfa651bd0d51087828
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-28 15:34:43 +0900

    feat: colorpalette 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,37 +1,41 @@
 import { DefaultTheme } from 'styled-components';
 
-export const darkTheme: DefaultTheme = {
-  background: '#111',
-  transparentBackground: 'rgba(43, 43, 43, 0.65)',
-  color: '#F5F6F7',
-  transparentColor: 'rgba(245, 246, 247, 0.65)',
+export const lightTheme: DefaultTheme = {
   pointColor: '#30B198',
-  pointColorLight: 'rgb(68, 197, 172)',
+  subColor: '#FFC857',
+  accentColor: '#EF476F',
+  background: '#F2F2F2',
+  transparentBackground: 'rgba(245, 246, 247, 0.65)',
+  color: '#333333',
+  transparentColor: 'rgba(43, 43, 43, 0.65)',
   transitionOption: 'ease-in-out 0.15s',
+  pointColorLight: 'rgb(68, 197, 172)',
 
   /* nav */
-  navBackground: '#222222',
+  navBackground: '#FFFFFF',
   navLinkBackground: '#D9D9D9',
-  borderColor: 'rgba(222,222,222,0.2)',
+  borderColor: '#000000',
 
   /* login */
   loginDisable: '#C9D5DB',
   loginBackground: '#F8FAFB',
 };
 
-export const lightTheme: DefaultTheme = {
-  background: '#F5F6F7',
-  transparentBackground: 'rgba(245, 246, 247, 0.65)',
-  color: '#2b2b2b',
-  transparentColor: 'rgba(43, 43, 43, 0.65)',
+export const darkTheme: DefaultTheme = {
   pointColor: '#30B198',
+  subColor: '#FFC857',
+  accentColor: '#EF476F',
   pointColorLight: 'rgb(68, 197, 172)',
+  background: '#2B2B2B',
+  transparentBackground: 'rgba(43, 43, 43, 0.65)',
+  color: '#F5F6F7',
+  transparentColor: 'rgba(245, 246, 247, 0.65)',
   transitionOption: 'ease-in-out 0.15s',
 
   /* nav */
-  navBackground: '#D9D9D9',
-  navLinkBackground: '#666666',
-  borderColor: '#000000',
+  navBackground: '#111',
+  navLinkBackground: '#D9D9D9',
+  borderColor: 'rgba(222,222,222,0.2)',
 
   /* login */
   loginDisable: '#C9D5DB',
