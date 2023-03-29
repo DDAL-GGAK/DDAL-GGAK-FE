58715533f7f45f42b83afcd55a002577a4fee7df
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-30 03:17:05 +0900

    feat: nav 컴포넌트 optionalRendering을 위한 HAS_NAV field 추가

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -5,25 +5,31 @@ export const ROUTE_MAP: RouteMap = Object.freeze({
   '/': {
     COMPONENT: <Home />,
     AUTH: false,
+    HAS_NAV: false,
   },
   '/signup': {
     COMPONENT: <Signup />,
     AUTH: false,
+    HAS_NAV: false,
   },
   '/login': {
     COMPONENT: <Login />,
     AUTH: false,
+    HAS_NAV: false,
   },
   '/project/:id': {
     COMPONENT: <Project />,
     AUTH: true,
+    HAS_NAV: true,
   },
   '/task/:id': {
     COMPONENT: <Task />,
     AUTH: true,
+    HAS_NAV: true,
   },
   '/user': {
     COMPONENT: <User />,
     AUTH: true,
+    HAS_NAV: true,
   },
 });
