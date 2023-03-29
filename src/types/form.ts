0d1cdd575a48b28db3071c570dd813b15b648ef4
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-29 15:45:08 +0900

    refactor: constant 기반 type 생성 적용

--- src/types/form.ts
+++ src/types/form.ts
@@ -1,8 +1,14 @@
 import { UseFormRegister } from 'react-hook-form';
-import { INPUT_TYPE } from 'constants/';
+import { INPUT_TYPE, REGISTER_TYPE } from 'constants/';
 
-export type InputTypes = (typeof INPUT_TYPE)[keyof typeof INPUT_TYPE];
-export type RegisterTypes = 'email' | 'password' | 'passwordConfirm';
+type InputSchema = typeof INPUT_TYPE;
+type InputKeys = keyof typeof INPUT_TYPE;
+export type InputTypes = InputSchema[InputKeys];
+
+type RegisterSchema = typeof REGISTER_TYPE;
+type RegisterKeys = keyof typeof REGISTER_TYPE;
+export type RegisterTypes = RegisterSchema[RegisterKeys];
+
 type RegisterForm = UseFormRegister<RegisterField>;
 type ErrorMessageState = string | undefined;
 type ErrorId = boolean;
@@ -32,3 +38,12 @@ export interface LabelProps {
 
 /* auth */
 export type RegisterField = Record<RegisterTypes, string>;
+
+/* Error */
+export interface ErrorMessage {
+  response: {
+    data: {
+      message: string;
+    };
+  };
+}
