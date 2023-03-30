c894a2ee27aa79d6a88239df9d739e4077421c8f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-31 04:22:02 +0900

    feat: auth 여부에 따른 optionalRendering 및 AuthRoute 적용

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,12 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import { ROUTE_MAP } from 'constants/';
+import { AuthRoute } from 'components';
 
 export function Router() {
   return (
     <Routes>
-      {Object.entries(ROUTE_MAP).map(([ROUTE, DATA]) => (
-        <Route key={ROUTE} path={ROUTE} element={DATA.COMPONENT} />
-      ))}
+      {Object.entries(ROUTE_MAP).map(([ROUTE, DATA]) => {
+        if (DATA.AUTH)
+          return (
+            <Route
+              key={ROUTE}
+              path={ROUTE}
+              element={<AuthRoute path={ROUTE} element={DATA.COMPONENT} />}
+            />
+          );
+        return <Route key={ROUTE} path={ROUTE} element={DATA.COMPONENT} />;
+      })}
     </Routes>
   );
 }
