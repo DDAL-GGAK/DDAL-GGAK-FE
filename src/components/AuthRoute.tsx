276d9aa4fe615a77e591231a4a5de784c4dbc718
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-31 13:45:06 +0900

    feat: authLoading에 따른 nav optionalRendering

--- src/components/AuthRoute.tsx
+++ src/components/AuthRoute.tsx
@@ -3,16 +3,21 @@ import { useState, useEffect } from 'react';
 import { Navigate, NavigateProps } from 'react-router-dom';
 import { AuthRouteProps } from 'types';
 import { ROUTE } from 'constants/';
+import { useDispatch } from 'react-redux';
+import { setAuthLoading } from 'redux/modules/authLoading';
 
 export function AuthRoute({ element, ...rest }: AuthRouteProps) {
   const [loading, setLoading] = useState(true);
   const [isAuth, setIsAuth] = useState(false);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const checkValidation = async () => {
+      dispatch(setAuthLoading(true));
       const auth = await checkAuth();
       setIsAuth(auth);
       setLoading(false);
+      dispatch(setAuthLoading(false));
     };
     checkValidation();
   }, []);
