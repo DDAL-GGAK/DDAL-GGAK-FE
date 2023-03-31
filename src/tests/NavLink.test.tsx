73ed0bbcdf8f3bf975acf8316b8885b968d35415
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-03-31 22:10:05 +0900

    test: navLink 테스트코드 작성

--- src/tests/NavLink.test.tsx
+++ src/tests/NavLink.test.tsx
@@ -2,30 +2,50 @@ import { render, screen } from '@testing-library/react';
 import { NavLink } from 'components';
 import { ProjectsLink } from 'types';
 import { BrowserRouter } from 'react-router-dom';
+// import { getUserProjects } from 'api';
 
 const mockProps: ProjectsLink = {
   id: 'mockID',
-  title: 'mockTitle',
+  projectTitle: 'mockTitle',
+  thumbnail: 'mockImage',
+};
+
+const mockPropsWithoutThumbnail: ProjectsLink = {
+  id: 'mockID',
+  projectTitle: 'mockTitle',
+  thumbnail: null,
 };
 
 describe('NavLink Component', () => {
-  test('props로 전달받은 id를 렌더링한다.', () => {
+  const { id, projectTitle, thumbnail } = mockProps;
+
+  test('클릭 시, 동적라우팅 된 page로 이동한다', () => {
     render(
       <BrowserRouter>
-        <NavLink navData={mockProps} />
+        <NavLink data={mockProps} />
       </BrowserRouter>
     );
-    const navId = screen.getByText(mockProps.id);
-    expect(navId).toBeInTheDocument();
+    const navLink = screen.getByRole('link');
+    expect(navLink).toHaveAttribute('href', `/project/${id}`);
   });
 
-  test('props로 전달받은 title을 렌더링한다.', () => {
+  test('thumbnail이 없는 경우, projectTitle의 첫 문자를 대문자로 렌더링한다.', () => {
     render(
       <BrowserRouter>
-        <NavLink navData={mockProps} />
+        <NavLink data={mockPropsWithoutThumbnail} />
       </BrowserRouter>
     );
-    const navTitle = screen.getByText(mockProps.title);
+    const navTitle = screen.getByText(projectTitle.toUpperCase()[0]);
     expect(navTitle).toBeInTheDocument();
+  });
+
+  test('thumbnail이 있는 경우, thumbnail 컴포넌트를 렌더링한다.', () => {
+    render(
+      <BrowserRouter>
+        <NavLink data={mockProps} />
+      </BrowserRouter>
+    );
+    const navThumnail = screen.getByRole('img');
+    expect(navThumnail).toHaveAttribute('src', thumbnail);
   });
 });
