7baa318194515c19528db5d86a7b954faa0f8cfe
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-01 19:14:34 +0900

    chore: modalCardVariant 수정

--- src/libs/variants.ts
+++ src/libs/variants.ts
@@ -21,29 +21,25 @@ const MODAL_EASE = 'easeInOut';
 export const modalCardVariants = {
   from: {
     opacity: 0,
-    x: -50,
+    y: 20,
     transition: {
       opacity: { duration: 0.15, ease: MODAL_EASE },
-      scale: { duration: 0.15, ease: MODAL_EASE },
       y: { duration: 0.15, ease: MODAL_EASE },
     },
   },
   to: {
     opacity: 1,
-    scale: 1,
-    x: 0,
+    y: 0,
     transition: {
       opacity: { duration: 0.15, ease: MODAL_EASE },
-      scale: { duration: 0.15, ease: MODAL_EASE },
       y: { duration: 0.15, ease: MODAL_EASE },
     },
   },
   exit: {
     opacity: 0,
-    x: 50,
+    y: 20,
     transition: {
       opacity: { duration: 0.15, ease: MODAL_EASE },
-      scale: { duration: 0.15, ease: MODAL_EASE },
       y: { duration: 0.15, ease: MODAL_EASE },
     },
   },
