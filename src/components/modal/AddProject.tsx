6684a32261876cdc36c5fc7c69b4cbdf5da14db2
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-01 18:41:16 +0900

    feat: addPorject 모달 추가

---
+++ src/components/modal/AddProject.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { AddModal, JoinModal } from 'components/modal';
+
+export function AddProjectModal() {
+  const [page, setPage] = useState(1);
+  const handlePrevClick = () => {
+    setPage(page - 1); // 이전 버튼을 클릭하면 페이지를 1 감소시킵니다.
+  };
+  const handleNextClick = () => {
+    setPage(page + 1); // 다음 버튼을 클릭하면 페이지를 1 증가시킵니다.
+  };
+  let content;
+  if (page === 1) {
+    content = <AddModal />;
+  } else if (page === 2) {
+    content = <JoinModal />;
+  }
+
+  return (
+    <div>
+      {content}
+      {page !== 1 && <Button onClick={handlePrevClick}>이전</Button>}
+      {page !== 2 && (
+        <div>
+          if you have invite code, please enter invitation code.
+          <br />
+          <Button onClick={handleNextClick}>Join Project</Button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+const Button = styled.button``;
