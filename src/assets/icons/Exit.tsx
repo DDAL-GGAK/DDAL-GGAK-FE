4ff1f63174d59e8bf1eb452d063f9272d05614ff
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-01 19:27:01 +0900

    feat: exit 컴포넌트 추가

---
+++ src/assets/icons/Exit.tsx
@@ -0,0 +1,19 @@
+export function Exit(size: number) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+      width={size}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  );
+}
