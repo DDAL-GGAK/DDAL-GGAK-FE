8e28505c66eebdf9fb0b2d44b1f514fb74b53df6
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-03 07:51:19 +0900

    refactor: projectTitleInput 분리

---
+++ src/components/form/ProjectTitleInput.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export function ProjectTitleInput({register}:any) {
+  return (
+    <Input
+      type="text"
+      placeholder="Enter your ProjectName"
+      {...register('projectTitle', {
+        required: 'Please enter your projectTitle!',
+        maxLength: {
+          value: 20,
+          message: 'Requires shorter than 20',
+        },
+      })}
+    />
+  );
+}
+
+const Input = styled.input`
+  padding: 0.5rem;
+  font-size: 14px;
+  border: 1px solid teal;
+  border-radius: 4px;
+`;
