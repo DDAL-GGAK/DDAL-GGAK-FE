cd37f6e555e8ca7ace2fdbe9b506f011e40ca748
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-04 05:35:54 +0900

    feat: back 컴포넌트 추가

---
+++ src/assets/icons/Back.tsx
@@ -0,0 +1,23 @@
+interface BackProps {
+  size: number;
+}
+
+export function Back({ size }: BackProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+      width={size}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 19.5L8.25 12l7.5-7.5"
+      />
+    </svg>
+  );
+}
