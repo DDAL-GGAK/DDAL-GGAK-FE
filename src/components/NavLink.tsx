f1e38f1e6788efb59b40b0af9abba0a0ede554de
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-03 19:10:14 +0900

    feat: 현재 project의 위치를 표시하는 navLink 컴포넌트 추가

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -5,27 +5,33 @@ import { ProjectsLink, Thumbnail } from 'types';
 
 interface NavLinkProps {
   data: ProjectsLink;
+  isCurrent: boolean;
 }
 
-export function NavLink({ data }: NavLinkProps) {
+export function NavLink({ data, isCurrent }: NavLinkProps) {
   const { id, projectTitle, thumbnail } = data;
+
+  console.log(isCurrent);
 
   return (
     <Link to={`/project/${id}`}>
+      {isCurrent && <Current />}
       {thumbnail ? (
-        <Wrapper thumbnail={thumbnail}>1</Wrapper>
+        <Wrapper thumbnail={thumbnail} />
       ) : (
-        <Wrapper>{projectTitle.toUpperCase()[0]}</Wrapper>
+        <Wrapper>
+          <Text>{projectTitle.toUpperCase()[0]}</Text>
+        </Wrapper>
       )}
     </Link>
   );
 }
 
 const Wrapper = styled.div<{ thumbnail?: Thumbnail }>`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
-  font-weight: 600;
   width: ${NAVLINK.WIDTH}px;
   height: ${NAVLINK.HEIGHT}px;
   border-radius: ${NAVLINK.BORDER_RADIUS}px;
@@ -33,16 +39,31 @@ const Wrapper = styled.div<{ thumbnail?: Thumbnail }>`
   color: ${({ theme }) => theme.background};
   background: ${(props) =>
     props.thumbnail
-      ? `url(${props.thumbnail}) center center / cover no-repeat`
+      ? `url(${props.thumbnail}) center / cover`
       : props.theme.navLinkBackground};
 
   :hover {
     cursor: pointer;
     background: ${(props) =>
       props.thumbnail
-        ? `url(${props.thumbnail}) center center / cover no-repeat`
+        ? `url(${props.thumbnail}) center / cover`
         : props.theme.color};
 
     border-radius: ${NAVLINK.HOVER_BORDER_RADIUS}px;
   }
+`;
+
+const Text = styled.div`
+  font-weight: 600;
+  color: teal;
+`;
+
+const Current = styled.div`
+  position: absolute;
+  left: 0px;
+  width: 5px;
+  border-radius: 0 10px 10px 0;
+  height: ${NAVLINK.HEIGHT}px;
+  transition: ${({ theme }) => theme.transitionOption};
+  background: ${({ theme }) => theme.pointColor};
 `;
