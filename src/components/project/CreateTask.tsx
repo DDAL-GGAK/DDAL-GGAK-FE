e48bdb0d83f4fb6659d10f83894818ed063af323
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-04 14:33:05 +0900

    refactor: 네이밍 컨벤션에 맞게 변경(addTask => createTask)

--- src/components/project/CreateTask.tsx
+++ src/components/project/CreateTask.tsx
@@ -2,8 +2,12 @@ import styled from 'styled-components';
 import { Add } from 'assets/icons';
 
 export function AddTask() {
+  const createTask = () => {
+    console.log(1123);
+  };
+
   return (
-    <Wrapper>
+    <Wrapper onClick={createTask}>
       <Add size={50} />
     </Wrapper>
   );
@@ -12,6 +16,12 @@ export function AddTask() {
 const Wrapper = styled.div`
   color: ${({ theme }) => theme.navBackground};
   transition: ${({ theme }) => theme.transitionOption};
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   :hover {
     cursor: pointer;
   }
