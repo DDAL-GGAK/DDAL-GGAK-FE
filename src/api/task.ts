be8d97b2f46dbc31f4ce545ab338b5e2590b5feb
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-06 00:25:09 +0900

    feat: taskData API 추가

--- src/api/task.ts
+++ src/api/task.ts
@@ -7,3 +7,7 @@ const api = new Axios(true);
 export const createTask = async (data: TaskCreateForm) => {
   await api.post(API_ROUTE.TASK.CREATE, data);
 };
+
+export const getTaskData = async (id: string) => {
+  await api.getByParams(API_ROUTE.TASK.GET_DATA, id);
+};
