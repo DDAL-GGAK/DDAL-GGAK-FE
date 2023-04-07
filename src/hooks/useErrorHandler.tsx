32011b58f8dc25fcb2fe77e6930a8acec16f7a81
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-07 22:56:37 +0900

    feat: errorHandler toast message 로직 추가

--- src/hooks/useErrorHandler.tsx
+++ src/hooks/useErrorHandler.tsx
@@ -1,6 +1,7 @@
 import { ERROR } from 'constants/';
 import { useNavigateBack } from 'hooks';
 import { AxiosError } from 'axios';
+import { sendToast } from 'libs';
 
 export function useErrorHandler() {
   const navigateBack = useNavigateBack();
@@ -9,6 +10,7 @@ export function useErrorHandler() {
     const error = err as AxiosError;
     console.log('handleError!');
 
+    sendToast.error(error.message);
     if (error.message === ERROR.FORBIDDEN.NO_ACCESS_RIGHTS)
       return navigateBack();
     if (error.message === ERROR.NOT_FOUND.USER_NOT_FOUND) return navigateBack();
