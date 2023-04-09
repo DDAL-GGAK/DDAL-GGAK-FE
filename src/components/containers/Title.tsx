5debe81bcdacf71136e1b352b109da9726d0ac22
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-10 06:38:36 +0900

    feat: title container 추가

---
+++ src/components/containers/Title.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { Container } from 'types';
+
+export function Title({ children, ...props }: Container) {
+  return <Wrapper {...props}>{children}</Wrapper>;
+}
+
+const Wrapper = styled.h2<Container>`
+  font-size: 24px;
+  font-weight: 600;
+  text-align: center;
+  color: ${({ theme }) => theme.color};
+`;
