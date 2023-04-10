fdc27cc9a0e23f8a22b92d20ddb78a64f03e9ff6
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-10 09:03:52 +0900

    feat: button 컴포넌트 style 분기 추가

--- src/components/containers/Button.tsx
+++ src/components/containers/Button.tsx
@@ -1,15 +1,30 @@
 import styled from 'styled-components';
 
 interface ButtonProps {
-  children: React.ReactNode;
+  buttonType?: 'point' | 'default';
+  children?: React.ReactNode;
   onClick?: () => void;
 }
 
-export function Button({ children, ...props }: ButtonProps) {
-  return <Wrapper {...props}>{children}</Wrapper>;
+export function Button({
+  buttonType = 'default',
+  children,
+  ...props
+}: ButtonProps) {
+  if (buttonType)
+    switch (buttonType) {
+      case 'point':
+        return <Point {...props}>{children}</Point>;
+      case 'default':
+        return <Default {...props}>{children}</Default>;
+      default:
+        return <Default {...props}>{children}</Default>;
+    }
+
+  return <Default {...props}>{children}</Default>;
 }
 
-const Wrapper = styled.button`
+const Default = styled.button`
   background: ${({ theme }) => theme.background};
   color: ${({ theme }) => theme.pointColor};
   font-weight: 600;
@@ -26,5 +41,21 @@ const Wrapper = styled.button`
     cursor: pointer;
     background: ${({ theme }) => theme.color};
     color: ${({ theme }) => theme.background};
+  }
+`;
+
+const Point = styled.button`
+  padding: 0.5rem 16px;
+  font-size: 14px;
+  font-weight: 600;
+  background: ${({ theme }) => theme.pointColor};
+  border: none;
+  border-radius: 4px;
+  transition: ${({ theme }) => theme.transitionOption};
+  color: whitesmoke;
+
+  :hover {
+    cursor: pointer;
+    background: ${({ theme }) => theme.pointColorLight};
   }
 `;
