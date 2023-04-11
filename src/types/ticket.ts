778078d31e646bc52c70fa0cfec31c02452e484a
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-11 16:05:57 +0900

    feat: TicketDetailForm type 추가

--- src/types/ticket.ts
+++ src/types/ticket.ts
@@ -4,6 +4,21 @@ export interface Tickets {
   DONE: [];
 }
 
+type TicketState = 'TODO' | 'IN_PROGRESS' | 'DONE';
+
+export interface TicketDetailForm {
+  assigned: string | null;
+  completedAt: string | null;
+  description: string;
+  difficulty: number | string;
+  expiredAt: string | null;
+  label: null | string;
+  priority: number | string;
+  status: TicketState;
+  ticketId: number | string;
+  title: string;
+}
+
 export interface TicketCreateForm {
   ticketTitle: string;
   ticketDescription: string;
