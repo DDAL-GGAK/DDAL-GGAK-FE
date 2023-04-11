0b4127cce3eb56df797195369834f19526248a7f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-11 14:59:39 +0900

    chore: configLabel 변경된 route 적용

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -4,4 +4,4 @@ export * from './JoinProject';
 export * from './ModalPortal';
 export * from './CreateTask';
 export * from './CreateLabel';
-export * from './ConfigmDelete';
+export * from './ConfirmDelete';
