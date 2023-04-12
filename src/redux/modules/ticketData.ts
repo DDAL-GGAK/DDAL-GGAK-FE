2a6f09a965467414fe098f2971a334732c5c9815
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-13 05:11:16 +0900

    feat: store ticketData label field 추가

--- src/redux/modules/ticketData.ts
+++ src/redux/modules/ticketData.ts
@@ -5,19 +5,36 @@ interface SetTicketDataAction {
   payload: Tickets;
 }
 
-const initialState: Tickets = {
-  TODO: [],
-  IN_PROGRESS: [],
-  DONE: [],
+interface SetLabelAction {
+  payload: string;
+}
+
+interface TicketState {
+  ticket: Tickets;
+  label: string;
+}
+
+const initialState: TicketState = {
+  ticket: {
+    TODO: [],
+    IN_PROGRESS: [],
+    DONE: [],
+  },
+  label: 'All',
 };
 
 const ticketDataSlicer = createSlice({
   name: 'ticketDataSlicer',
   initialState,
   reducers: {
-    setTicketData: (state, action: SetTicketDataAction) => action.payload,
+    setTicketData: (state, action: SetTicketDataAction) => {
+      return { ...state, ticket: action.payload };
+    },
+    setLabel: (state, action: SetLabelAction) => {
+      return { ...state, label: action.payload };
+    },
   },
 });
 
 export default ticketDataSlicer.reducer;
-export const { setTicketData } = ticketDataSlicer.actions;
+export const { setTicketData, setLabel } = ticketDataSlicer.actions;
