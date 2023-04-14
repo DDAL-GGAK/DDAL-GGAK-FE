909ed49b6aa4470f992379d27daef20e3a36225f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-14 20:44:11 +0900

    feat: content1 컴포넌트 type animation적용

--- src/components/home/Content1.tsx
+++ src/components/home/Content1.tsx
@@ -1,28 +1,116 @@
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import mainVideo from 'assets/video/task.mp4';
+
+const textList = ['tickets', 'tasks', 'projects'];
 
 export function Content1() {
+  const [textIndex, setTextIndex] = useState(0);
+  const [displayText, setDisplayText] = useState('');
+  const [isTyping, setIsTyping] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(
+      () => {
+        const currentText = textList[textIndex];
+        const currentLength = displayText.length;
+
+        if (isTyping) {
+          if (currentLength < currentText.length) {
+            setDisplayText(currentText.slice(0, currentLength + 1));
+          } else {
+            setIsTyping(false);
+          }
+        } else if (currentLength > 0) {
+          setDisplayText(currentText.slice(0, currentLength - 1));
+        } else {
+          setIsTyping(true);
+          setTextIndex((prevIndex) => (prevIndex + 1) % textList.length);
+        }
+      },
+      isTyping ? 100 : 100
+    );
+
+    return () => clearTimeout(timer);
+  }, [displayText, isTyping, textIndex]);
+
   return (
     <Wrapper id="content_1">
-      <div>string</div>
-      <div>
-        <Point>text</Point> text
-      </div>
+      <VideoContainer>
+        <Video autoPlay loop muted>
+          <source src={mainVideo} type="video/mp4" />
+        </Video>
+        <Overlay />
+        <Content>
+          <Title>
+            Welcome to <Point>DDAL-GGAK</Point>
+          </Title>
+          <SubTitle>
+            Manage your <Point>{displayText}</Point> efficiently
+          </SubTitle>
+        </Content>
+      </VideoContainer>
     </Wrapper>
   );
 }
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const VideoContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Video = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.6) 0%,
+    rgba(0, 0, 0, 0.6) 100%
+  );
+  mix-blend-mode: multiply;
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  color: #f3f3f3;
+  padding: 5rem;
+  border-radius: 10px;
+  background: rgba(255, 255, 255, 0.15);
+  backdrop-filter: blur(3px);
+`;
+
+const Title = styled.div`
+  font-weight: 600;
+  font-size: 3rem;
+  margin-bottom: 1rem;
+`;
+
+const SubTitle = styled.div`
+  font-size: 1.5rem;
+`;
 
 const Point = styled.span`
-  color: ${({ theme }) => theme.pointColor};
-`;
-const Wrapper = styled.div`
-  display: flex;
-  font-size: 100px;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  color: white;
-  gap: 30px;
   font-weight: 600;
-  height: 100vh;
-  background: #111;
+  color: ${({ theme }) => theme.pointColor};
 `;
