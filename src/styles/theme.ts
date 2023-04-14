0a3e90f800e552cafae00cc3ec3d5f127ad95e3c
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-14 22:03:39 +0900

    chore: theme 색상 변경

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -29,9 +29,9 @@ export const darkTheme: DefaultTheme = {
   subColor: '#FFC857',
   accentColor: '#EF476F',
   pointColorLight: 'rgb(68, 197, 172)',
-  background: '#2B2B2B',
-  transparentBackground: 'rgba(43, 43, 43, 0.65)',
-  color: '#F5F6F7',
+  background: '#191A23',
+  transparentBackground: '#21232E',
+  color: '#E2E2E2',
   transparentColor: 'rgba(245, 246, 247, 0.65)',
   transitionOption: 'ease-in-out 0.15s',
   boxShadow: '0 2px 4px rgba(0, 0, 0, 0.5)',
@@ -39,7 +39,7 @@ export const darkTheme: DefaultTheme = {
   /* nav */
   navBackground: '#111',
   navLinkBackground: '#D9D9D9',
-  borderColor: 'rgba(222,222,222,0.2)',
+  borderColor: '#2C2D3C',
 
   /* login */
   loginDisable: '#C9D5DB',
