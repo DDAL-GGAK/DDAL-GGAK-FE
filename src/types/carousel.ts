8aca879243b1de49b416d43cd1deaedf45465fac
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-14 18:05:33 +0900

    feat: carousel Type 추가

--- src/types/carousel.ts
+++ src/types/carousel.ts
@@ -10,3 +10,17 @@ export interface CarouselComponentProps {
   imgSrc: string;
   textData: TextDataType;
 }
+
+export interface CarouselTextProps {
+  textData: TextDataType;
+}
+
+interface ImgsState extends TextDataType {
+  src: string;
+}
+
+export interface CarouselState {
+  index: number;
+  imgs: ImgsState[];
+  intervalRef: NodeJS.Timer | any;
+}
