acb2657610c3d2ada5a446192bd748a12b31b3b0
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-15 12:13:47 +0900

    feat: radio 공통 컴포넌트 추가

---
+++ src/components/form/RadioComponent.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const RadioGroup = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const RadioLabel = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  color: ${({ theme }) => theme.color};
+  font-size: 14px;
+`;
+
+export const RadioInput = styled.input`
+  appearance: none;
+  width: 16px;
+  height: 16px;
+  background-color: ${({ theme }) => theme.transparentBackground};
+  border: 1px solid ${({ theme }) => theme.color};
+  border-radius: 50%;
+  outline: none;
+
+  &:checked {
+    background-color: ${({ theme }) => theme.pointColor};
+    border-color: ${({ theme }) => theme.pointColor};
+  }
+
+  &:checked::after {
+    content: '';
+    display: block;
+    width: 8px;
+    height: 8px;
+    background-color: ${({ theme }) => theme.background};
+    border-radius: 50%;
+    position: relative;
+    left: 3px;
+    top: 3px;
+  }
+`;
