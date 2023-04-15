0909bace2a33bd667f5dfbaca2d026361cdcf820
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-16 04:02:14 +0900

    feat: setting Route 추가

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -2,4 +2,9 @@ export const ROUTE = Object.freeze({
   HOME: '/',
   LOGIN: '/login',
   PROJECT_HOME: '/project/',
+  SETTING: Object.freeze({
+    USER: 'user',
+    PROJECT_SETTING: 'projectSetting',
+    PROJECT_MEMBER: 'projectMember',
+  }),
 });
