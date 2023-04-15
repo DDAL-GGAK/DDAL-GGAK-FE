a7d29c192b7dc2adc1a64e5bc11b3860c8283693
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-15 12:45:27 +0900

    chore: title 컴포넌트 CSS 수정

--- src/components/containers/Title.tsx
+++ src/components/containers/Title.tsx
@@ -9,5 +9,8 @@ const Wrapper = styled.h2<Container>`
   font-size: 24px;
   font-weight: 600;
   text-align: left;
+  display: flex;
+  align-items: center;
+  gap: 10px;
   color: ${({ theme }) => theme.color};
 `;
