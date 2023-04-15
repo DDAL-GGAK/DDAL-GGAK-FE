7b801b52251a5a223edf6e080283e28c777ee6a4
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-15 13:59:57 +0900

    chore: 간단한 CSS 변경

--- src/components/project/LabelConfigButton.tsx
+++ src/components/project/LabelConfigButton.tsx
@@ -3,7 +3,7 @@ import { MODAL_CARD_VARIANTS } from 'constants/';
 import { LabelButton } from 'components';
 import { Config } from 'assets/icons';
 import { LabelsProps } from 'types';
-import { TaskConfig } from 'components/modal/TaskConfig';
+import { TaskConfig } from 'components/task';
 
 export function LabelConfigButton({ labels }: LabelsProps) {
   const { Modal, isOpen, openModal, closeModal } = useModal();
