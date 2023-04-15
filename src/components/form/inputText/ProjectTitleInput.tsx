dae7d3853d010e6eb71ce04f7d4c574e65f28de3
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-16 03:14:14 +0900

    chore: placeholder 변경

--- src/components/form/inputText/ProjectTitleInput.tsx
+++ src/components/form/inputText/ProjectTitleInput.tsx
@@ -6,7 +6,7 @@ export function ProjectTitleInput({ register }: ProjectTitleInputProps) {
   return (
     <TextInput
       type="text"
-      placeholder="Your ProjectName"
+      placeholder="Task Name"
       register={register(REGISTER_TYPE.PROJECT_TITLE, {
         required: ERROR_MESSAGE.PROJECT_TITLE.REQUIRED,
         maxLength: {
