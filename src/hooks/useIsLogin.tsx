58e83c164540c922b27cca439bca0c2814380fea
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-15 15:45:22 +0900

    feat: useIsLoin 로직 변경(localStorage Data & store 사용)

--- src/hooks/useIsLogin.tsx
+++ src/hooks/useIsLogin.tsx
@@ -1,8 +1,10 @@
 import { useSelector } from 'react-redux';
 import { RootState } from 'redux/store';
+import { QUERY } from 'constants/';
 
 export const useIsLogin = () => {
-  const isLogin = useSelector((state: RootState) => state.userDataSlicer);
+  const storeData = !!useSelector((state: RootState) => state.userDataSlicer);
+  const localStorageData = !!localStorage.getItem(QUERY.KEY.USER_DATA);
 
-  return !!isLogin;
+  return storeData || localStorageData;
 };
