a52bae3e2dbf223a4c7cf6ec3855a7aa834e69e6
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-15 12:11:21 +0900

    refactor: ticketPriority 컴포넌트 분리

---
+++ src/components/form/ticket/TicketPriorityInput.tsx
@@ -0,0 +1,35 @@
+import { REGISTER_TYPE, ERROR_MESSAGE, CONFIG } from 'constants/';
+import { TicketCreateRegister } from 'types';
+import { LabelWrapper, LabelText, ErrorMessage } from 'components/containers';
+import { RadioInput, RadioLabel, RadioGroup } from '../RadioComponent';
+
+export function TicketPriorityInput({
+  register,
+  errors,
+}: TicketCreateRegister) {
+  return (
+    <>
+      <LabelWrapper>
+        <LabelText>Difficulty:</LabelText>
+        {errors.ticketDifficulty && (
+          <ErrorMessage>{errors.ticketDifficulty.message}</ErrorMessage>
+        )}
+      </LabelWrapper>
+      <RadioGroup>
+        {Array.from({ length: CONFIG.TICKET_DIFFICULTY.LENGTH }, (_, i) => (
+          <RadioLabel key={`difficulty${i + 1}`}>
+            <RadioInput
+              type="radio"
+              id={`difficulty${i + 1}`}
+              value={i + 1}
+              {...register(REGISTER_TYPE.TICKET_DIFFICULTY, {
+                required: ERROR_MESSAGE.TICKET_DIFFICULTY.REQUIRED,
+              })}
+            />
+            <label htmlFor={`difficulty${i + 1}`}>{i + 1}</label>
+          </RadioLabel>
+        ))}
+      </RadioGroup>
+    </>
+  );
+}
