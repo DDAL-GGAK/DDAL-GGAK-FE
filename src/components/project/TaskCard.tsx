1a108179eeb826fde234187728404234a704f04e
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-16 08:20:54 +0900

    feat: taskCard CSS 추가

--- src/components/project/TaskCard.tsx
+++ src/components/project/TaskCard.tsx
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 import { TaskDataForm } from 'types';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { calcDeadlinePercentage } from 'utils';
 import { memo } from 'react';
+import { ContentText } from 'components/containers';
+import { TicketIcon } from '@heroicons/react/24/outline';
 
 interface TaskCardProps {
   taskData: TaskDataForm;
@@ -13,6 +14,7 @@ export const TaskCard = memo(({ taskData }: TaskCardProps) => {
   const {
     id,
     participantsCount,
+    participants,
     taskTitle,
     completedTickets,
     totalTickets,
@@ -21,44 +23,111 @@ export const TaskCard = memo(({ taskData }: TaskCardProps) => {
   } = taskData;
 
   const progressPercentage = (completedTickets / totalTickets) * 100;
-  const expired = new Date(expiredAt) < new Date();
-  const deadLinePercentage = calcDeadlinePercentage({ expiredAt, createdAt });
 
+  const dummy = [
+    {
+      id: 1,
+      email: 'string',
+      nickname: 'string',
+      thumbnail: 'string',
+      role: 'string',
+    },
+    {
+      id: 2,
+      email: 'string',
+      nickname: 'string',
+      thumbnail: 'string',
+      role: 'string',
+    },
+    {
+      id: 3,
+      email: 'string',
+      nickname: 'string',
+      thumbnail: 'string',
+      role: 'string',
+    },
+    {
+      id: 4,
+      email: 'string',
+      nickname: 'string',
+      thumbnail: 'string',
+      role: 'string',
+    },
+    {
+      id: 4,
+      email: 'string',
+      nickname: 'string',
+      thumbnail: 'string',
+      role: 'string',
+    },
+    {
+      id: 7,
+      email: 'string',
+      nickname: 'string',
+      thumbnail: 'string',
+      role: 'string',
+    },
+    {
+      id: 5,
+      email: 'string',
+      nickname: 'string',
+      thumbnail: 'string',
+      role: 'string',
+    },
+    {
+      id: 6,
+      email: 'string',
+      nickname: 'string',
+      thumbnail: 'string',
+      role: 'string',
+    },
+  ];
   return (
     <MyLink to={`./task/${id}`}>
-      <Wrapper data-expired={expired} key={id}>
-        <Title>{taskTitle}</Title>
-        <Info>
-          <div>Participants: {participantsCount}</div>
-        </Info>
-        <Hr />
+      <Wrapper key={id}>
+        <TopWrapper>
+          <TitleWrapper>
+            <Title>{taskTitle}</Title>
+          </TitleWrapper>
+          <Info>
+            <ContentText>Participants: {participantsCount}</ContentText>
+            <ImageWrapper>
+              {[...participants, ...dummy].slice(0, 5).map((v, i) => (
+                <Image src={v.thumbnail} index={i} key={v.id} alt="img" />
+              ))}
+            </ImageWrapper>
+          </Info>
+        </TopWrapper>
         <BottomWrapper>
+          <Tickets>
+            <ContentText>Tickets</ContentText>
+            <TicketIconWrapper>
+              {`${completedTickets} / ${totalTickets}`}
+              <TicketIcon style={{ width: 25 }} />
+            </TicketIconWrapper>
+          </Tickets>
           <ProgressBar>
             <ProgressFiller progress={progressPercentage} />
           </ProgressBar>
-          <Tickets>{`${completedTickets} / ${totalTickets}`} Tickets</Tickets>
-          <DeadlineBar>
-            <DeadlineFiller deadLine={deadLinePercentage} />
-          </DeadlineBar>
-          <div>
-            <div>Start: {createdAt}</div>
-            <div>Due: {expiredAt}</div>
-          </div>
+          <TimeWrapper>
+            <Time>{createdAt}</Time>
+            <Time>D-{expiredAt}</Time>
+          </TimeWrapper>
         </BottomWrapper>
       </Wrapper>
     </MyLink>
   );
 });
 
-const Wrapper = styled(motion.div)<{ 'data-expired': boolean }>`
+const Wrapper = styled(motion.div)`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   gap: 15px;
   border-radius: 8px;
   min-width: 300px;
   color: ${({ theme }) => theme.color};
-  background: ${({ 'data-expired': expired, theme }) =>
-    expired ? theme.transparentColor : theme.taskCardBackground};
+  background: ${({ theme }) => theme.taskCardBackground};
   box-shadow: ${({ theme }) => theme.boxShadow};
   height: 240px;
   padding: 20px;
@@ -71,15 +140,26 @@ const Wrapper = styled(motion.div)<{ 'data-expired': boolean }>`
   }
 `;
 
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const MyLink = styled(Link)`
   border-radius: 8px;
   transition: ${({ theme }) => theme.transitionOption};
   min-width: 300px;
 `;
 
+const TopWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const Title = styled.div`
   margin: 0;
-  font-size: 1.4rem;
+  font-size: 1.75rem;
   font-weight: 600;
   color: ${({ theme }) => theme.pointColor};
 `;
@@ -93,14 +173,9 @@ const Info = styled.div`
   gap: 0.5rem;
 `;
 
-const Hr = styled.div`
-  width: 100%;
-  border-bottom: 1px solid rgba(122, 122, 122, 0.5);
-`;
-
 const ProgressBar = styled.div`
   width: 100%;
-  height: 10px;
+  height: 6px;
   background-color: ${({ theme }) => theme.background};
   border-radius: 5px;
   margin: 0.5rem 0;
@@ -114,8 +189,24 @@ const ProgressFiller = styled.div<{ progress: number }>`
 `;
 
 const Tickets = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  gap: 8px;
   font-size: 0.9rem;
   font-weight: 400;
+`;
+
+const TicketIconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 20px;
+
+  svg {
+    color: ${({ theme }) => theme.subColor};
+  }
 `;
 
 const BottomWrapper = styled.div`
@@ -124,17 +215,34 @@ const BottomWrapper = styled.div`
   align-items: flex-end;
 `;
 
-const DeadlineBar = styled.div`
-  width: 100%;
-  height: 5px;
-  background-color: ${({ theme }) => theme.background};
-  border-radius: 5px;
-  margin: 0.5rem 0;
+const ImageWrapper = styled.div`
+  position: relative;
 `;
 
-const DeadlineFiller = styled.div<{ deadLine: number }>`
-  width: ${({ deadLine }) => deadLine}%;
-  height: 100%;
-  background-color: ${({ theme }) => theme.accentColor};
-  border-radius: 5px;
+const Image = styled.img<{ index: number }>`
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  border: 2px solid ${({ theme }) => theme.background};
+  right: ${({ index }) => index * 25}px;
+  background: white;
+`;
+
+const TimeWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem 0;
+  width: 100%;
+  margin-top: 0.5rem;
+`;
+
+const Time = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px 10px;
+  background: #575868;
+  border-radius: 15px;
+  color: white;
 `;
