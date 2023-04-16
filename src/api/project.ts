f3efe41f224a640752e4ac39be9a3a9f8c92e33b
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-16 18:46:50 +0900

    feat: createProjectInviteCode API 추가

--- src/api/project.ts
+++ src/api/project.ts
@@ -17,7 +17,10 @@ export const createProject = async (data: FormData) => {
 };
 
 export const joinProject = async (projectId: string | number) => {
-  const res = await api.post(API_ROUTE.PROJECT.JOIN_PROJECT(projectId), {});
+  const res = await api.post(
+    API_ROUTE.PROJECT.JOIN_PROJECT(projectId as string),
+    {}
+  );
 
   return res;
 };
@@ -31,21 +34,39 @@ export const getProjectData = async (param: string) => {
   return res;
 };
 
-export const updateProjectThumbnail = async ({data, projectId}: UpdateProjectDataProps) => {
-  const res = await api.postMultipartFormData(API_ROUTE.PROJECT.SET_THUMBNAIL(projectId), data as FormData);
+export const updateProjectThumbnail = async ({
+  data,
+  projectId,
+}: UpdateProjectDataProps) => {
+  const res = await api.postMultipartFormData(
+    API_ROUTE.PROJECT.SET_THUMBNAIL(projectId as string),
+    data as FormData
+  );
 
   return res;
 };
 
-export const updateProjectTitle = async ({data, projectId}: UpdateProjectDataProps) => {
+export const updateProjectTitle = async ({
+  data,
+  projectId,
+}: UpdateProjectDataProps) => {
   if (!data) return;
-  const res = await api.postMultipartFormData(API_ROUTE.PROJECT.SET_TITLE(projectId), data as FormData);
+  const res = await api.postMultipartFormData(
+    API_ROUTE.PROJECT.SET_TITLE(projectId as string),
+    data as FormData
+  );
 
   return res;
 };
 
-export const deleteProject = async (projectId: string | number) => {
+export const deleteProject = async (projectId: string) => {
   const res = await api.delete(API_ROUTE.PROJECT.DELETE, projectId);
+
+  return res;
+};
+
+export const createProjectInviteCode = async (projectId: string) => {
+  const res = await api.get(API_ROUTE.PROJECT.CREATE_INVITE_CODE(projectId));
 
   return res;
 };
