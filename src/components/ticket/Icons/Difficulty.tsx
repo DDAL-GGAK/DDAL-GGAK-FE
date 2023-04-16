23fa7c79f1a268c9a1f3fd189917c77662075f38
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-17 03:41:37 +0900

    feat: difficulty asset 및 색상 적용 로직 추가

--- src/components/ticket/Icons/Difficulty.tsx
+++ src/components/ticket/Icons/Difficulty.tsx
@@ -1,19 +1,14 @@
 import { LabelText } from 'components/containers';
 import styled from 'styled-components';
-import l1 from 'assets/img/l1.png';
-import l2 from 'assets/img/l2.png';
-import l3 from 'assets/img/l3.png';
-import l4 from 'assets/img/l4.png';
-import l5 from 'assets/img/l5.png';
+import { DIFFICULTY } from 'constants/';
 
 interface DifficultyProps {
   difficulty: number;
 }
 
 export function Difficulty({ difficulty }: DifficultyProps) {
-  const levelMap = [0, l1, l2, l3, l4, l5];
+  const src = DIFFICULTY[difficulty];
 
-  const src = levelMap[difficulty];
   return (
     <Wrapper>
       <Image src={src} alt="level" />
@@ -29,12 +24,11 @@ const Wrapper = styled.div`
   width: 2rem;
   font-size: 14px;
   padding: 4px 8px;
-  background: ${({ theme }) => theme.borderColor};
   color: white;
   border-radius: 4px;
   gap: 5px;
 `;
 
-const Image = styled.img<{ src: string }>`
+const Image = styled.img`
   width: 20px;
 `;
