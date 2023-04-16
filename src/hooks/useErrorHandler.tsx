388dde49d526a33a0c76196f833c7d3b39cf3fc1
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-17 05:51:09 +0900

    feat: errorHandler 401 에러 핸들링 적용

--- src/hooks/useErrorHandler.tsx
+++ src/hooks/useErrorHandler.tsx
@@ -1,7 +1,10 @@
-import { ERROR, ROUTE } from 'constants/';
+import { COOKIE, ERROR, QUERY, ROUTE, STATUS_CODES } from 'constants/';
 import { AxiosError } from 'axios';
 import { sendToast } from 'libs';
 import { useNavigate } from 'react-router-dom';
+import { Cookies } from 'react-cookie';
+import { useDispatch } from 'react-redux';
+import { removeUserData } from 'redux/modules/userData';
 
 interface RouteForm {
   route: string | null | undefined;
@@ -11,6 +14,9 @@ export function useErrorHandler({ route }: RouteForm = { route: null }) {
   const navigate = useNavigate();
   const defaultRoute = ROUTE.HOME;
   const moveTo = route || defaultRoute;
+  const cookie = new Cookies();
+  const dispatch = useDispatch();
+
   const errorHandler = (err: unknown) => {
     const error = err as AxiosError;
     sendToast.error(error.message);
@@ -19,6 +25,15 @@ export function useErrorHandler({ route }: RouteForm = { route: null }) {
       return navigate(moveTo);
     if (error.message === ERROR.NOT_FOUND.USER_NOT_FOUND)
       return navigate(moveTo);
+    if (
+      (error?.response?.status as number) === STATUS_CODES.ERROR.UNAUTHORIZED
+    ) {
+      cookie.remove(COOKIE.KEY.ACCESS_TOKEN, {
+        ...COOKIE.CONFIG.DEFAULT,
+      });
+      localStorage.removeItem(QUERY.KEY.USER_DATA);
+      dispatch(removeUserData());
+    }
     if ((error?.response?.status as number) >= 400) return navigate(moveTo);
   };
 
