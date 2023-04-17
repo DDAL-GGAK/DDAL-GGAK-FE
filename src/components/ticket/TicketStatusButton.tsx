b86a7e59957f9d42be10094cccbac6b048e7da71
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-18 01:15:58 +0900

    refactor: ticketStatus button content 로직 분리

--- src/components/ticket/TicketStatusButton.tsx
+++ src/components/ticket/TicketStatusButton.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Button } from 'components/containers';
 import { TICKET } from 'constants/';
 
-export const TicketStatusButton = styled(Button)<{ status: string }>`
+export const TicketButton = styled(Button)<{ status: string }>`
   position: relative;
   padding: 1rem 2rem;
   border: ${({ theme }) => theme.borderColor} 1px solid;
@@ -10,19 +10,7 @@ export const TicketStatusButton = styled(Button)<{ status: string }>`
   background: ${({ theme }) => theme.background};
   color: ${({ theme }) => theme.color};
 
-  /* Button Text */
-  --defaultText: ${({ status }) => `"${status}"`};
-  --hoverText: ${({ status }) => {
-    const { TODO, IN_PROGRESS } = TICKET.STATUS;
-
-    if (status === TODO) return `"${IN_PROGRESS}"`;
-    if (status === IN_PROGRESS) return `"${TODO}"`;
-
-    return `"${status}"`;
-  }};
-
   ::before {
-    content: var(--defaultText);
     position: absolute;
     top: 0;
     left: 0;
@@ -35,7 +23,6 @@ export const TicketStatusButton = styled(Button)<{ status: string }>`
   }
 
   ::after {
-    content: var(--hoverText);
     position: absolute;
     top: 0;
     left: 0;
@@ -59,5 +46,25 @@ export const TicketStatusButton = styled(Button)<{ status: string }>`
     ::before {
       opacity: 0;
     }
+  }
+`;
+
+export const TicketStatusButton = styled(TicketButton)<{ status: string }>`
+  --defaultText: ${({ status }) => `"${status}"`};
+  --hoverText: ${({ status }) => {
+    const { TODO, IN_PROGRESS } = TICKET.STATUS;
+
+    if (status === TODO) return `"${IN_PROGRESS}"`;
+    if (status === IN_PROGRESS) return `"${TODO}"`;
+
+    return `"${status}"`;
+  }};
+
+  ::before {
+    content: var(--defaultText);
+  }
+
+  ::after {
+    content: var(--hoverText);
   }
 `;
