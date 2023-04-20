868cb344676a0589d4a1132943416e4a4b70014e
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-20 20:30:06 +0900

    chore: ticket 현황 컴포넌트 overflow-x 추가

--- src/shared/Profile.tsx
+++ src/shared/Profile.tsx
@@ -19,7 +19,7 @@ export function Profile() {
 
   return (
     <div>
-      {data ? (
+      {data?.profile ? (
         <ProfileImage
           profile={data.profile}
           variants={DEFAULT_VARIANTS}
@@ -59,7 +59,7 @@ const DefaultImage = styled(motion.div)`
   color: ${({ theme }) => theme.borderColor};
   width: 40px;
   height: 40px;
-  border-radius: 8px;
+  border-radius: 50%;
   display: flex;
   align-items: center;
   justify-content: center;
