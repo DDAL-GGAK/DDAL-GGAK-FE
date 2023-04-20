c21f9679205647c74d29abcb0647836c08192086
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-20 16:12:08 +0900

    feat: homeLogo link 적용

--- src/shared/MainLogo.tsx
+++ src/shared/MainLogo.tsx
@@ -2,18 +2,21 @@ import styled from 'styled-components';
 import { Logo } from 'assets/icons';
 import { SVG_SIZE, DEFAULT_VARIANTS } from 'constants/';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 export function MainLogo() {
   return (
-    <Wrapper
-      variants={DEFAULT_VARIANTS}
-      initial="from"
-      animate="to"
-      exit="exit"
-    >
-      <Logo size={SVG_SIZE.LOGO_SIZE} />
-      <LogoText>DDAL-GGAK</LogoText>
-    </Wrapper>
+    <Link to={'/'}>
+      <Wrapper
+        variants={DEFAULT_VARIANTS}
+        initial="from"
+        animate="to"
+        exit="exit"
+      >
+        <Logo size={SVG_SIZE.LOGO_SIZE} />
+        <LogoText>DDAL-GGAK</LogoText>
+      </Wrapper>
+    </Link>
   );
 }
 
