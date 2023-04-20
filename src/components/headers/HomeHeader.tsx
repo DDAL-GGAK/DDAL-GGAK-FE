9da7d7a593046e33e6fe26bed5319b1be69e15c0
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-04-21 01:19:37 +0900

    feat: title명 통일

--- src/components/headers/HomeHeader.tsx
+++ src/components/headers/HomeHeader.tsx
@@ -1,11 +1,11 @@
-import { DefaultHeader } from './DefaultHeader';
+import { DefaultHeader } from 'components/headers';
 
 export function HomeHeader() {
   const pageTitle = 'DDal-GGak - Team Task Management';
 
   return (
     <DefaultHeader>
-      <title>Home</title>
+      <title>{pageTitle}</title>
       <meta property="og:title" content={pageTitle} />
       <meta name="twitter:title" content={pageTitle} />
     </DefaultHeader>
